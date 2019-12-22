b6080bb0917f9d277a8dda0bdd17da46a3c34135
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-12-22 08:18:23 +0100

    maintenance: bring ish up to date

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 import {
   encode,
   decode
-} from "https://denopkg.com/chiefbiiko/std-encoding/mod.ts";
+} from "https://denopkg.com/chiefbiiko/std-encoding@v1.0.0/mod.ts";
 
 /** Byte length of a SHA512 hash. */
 export const BYTES: number = 64;
